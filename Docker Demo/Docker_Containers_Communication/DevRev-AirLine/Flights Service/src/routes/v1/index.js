d026e87a619bef28ebc16501f3889449b470cbd4
Arijit Sarkar <arijitsrkr13@gmail.com>
2024-01-18 00:31:01 +0530

    Docker Updated

---
+++ Docker Demo/Docker_Containers_Communication/DevRev-AirLine/Flights Service/src/routes/v1/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { InfoController } = require("../../controllers");
+const airplaneRoutes = require("./airplane-routes");
+const cityRoutes = require("./city-routes");
+const airportRoutes = require("./airport-routes");
+const flightRoutes = require("./flight-routes");
+const axios = require("axios");
+
+/*
+router.get("/info", (req, res) => {
+  // Link : http://localhost:3000/api/v1/info
+  return res.json({ ms: "OK" });
+});
+*/
+// Replace the above code using this current clean code
+
+router.use("/airplanes", airplaneRoutes);
+router.use("/cities", cityRoutes);
+router.use("/airports", airportRoutes);
+router.use("/flights", flightRoutes);
+
+router.get("/info", InfoController.info);
+
+
+router.get("/callingPythonService", async (req, res) => {
+  const response = await axios.get("http://pythonService:3005/home");
+  return res.json({ message: response.data });
+});
+module.exports = router;
